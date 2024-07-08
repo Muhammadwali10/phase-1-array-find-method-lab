5823f238783a460bcf2226061773dac3436e7993
Muhammadwali10 <mohamedmowlid224@yahoo.com>
2024-07-08 07:58:34 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const Win = record.find((record) => record.result === "W");
+    if (Win) {
+        return Win.year;
+    } else {
+        return undefined;
+    }
+}
+
